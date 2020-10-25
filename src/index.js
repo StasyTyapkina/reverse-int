8be24b552fc3e7b61b79158065f8f75a2ab69e5a
StasyTyapkina <stasya.tyapkina@gmail.com>
2020-10-25 16:05:36 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let p;
+let a =  n.toString().split('').reverse().join('');
+    if (a[a.length-1]==='-'){
+        p = a.slice(0, a.length-1);
+    } else {p = a}
+    return p;
 }
